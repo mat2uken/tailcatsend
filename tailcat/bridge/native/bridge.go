8950e49a933b48f8b47d9f5ce3719ebea3094e1b
Kenichi Matsumoto <Kenichi.A.Matsumoto+rja@roland.com>
2026-09-08 22:29:14 +0900

    fix: add socket headers for iOS Go build and use public URL for web invites

--- tailcat/bridge/native/bridge.go
+++ tailcat/bridge/native/bridge.go
@@ -5,6 +5,10 @@ package main
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <ifaddrs.h>
 #include <net/if.h>
 
